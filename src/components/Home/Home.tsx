3e54eac5dcef5c38b344217918e53d00cf136eb8
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-29 08:09:31 -0300

    add animate.css and add typewriter style home title

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import styles from "./styles.css"
+import styles from "./Home.module.css";
 
 
 export default function Home() {
@@ -8,7 +8,7 @@ export default function Home() {
             <h1 className="text-3xl md:text-5xl text-start mx-10 mt-10 font-semibold text-white tracking-wide leading-tight">
                 Hello! <br/>
             </h1>
-            <h2 className="text-3xl md:text-5xl text-start mx-10 mb-10 font-semibold text-white tracking-wide leading-tight">I am
+            <h2 className={`${styles.typewriter} text-3xl md:text-5xl text-start mx-10 mb-10 font-semibold text-white tracking-wide leading-tight`}>I am
                 Victor Mazzola!</h2>
 
 
