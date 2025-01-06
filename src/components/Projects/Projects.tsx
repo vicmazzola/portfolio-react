1cff795b3aa22b0b716e7c0034e0a32b8779c2a7
Victor Mazzola <victormazzola2019@gmail.com>
2025-01-06 18:18:49 -0300

    change projects to xl:grid-cols-4

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -150,7 +150,7 @@ export default function Projects() {
 
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 xl:grid-cols-3 gap-8 py-12 mx-6 my-2 overflow-x-hidden">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-8 py-12 mx-6 my-2 overflow-x-hidden">
             {cards.map((card, index) => (
                 <Card
                     key={index}
